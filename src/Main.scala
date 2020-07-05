0e6ddbe12111283eb896af14d795f04124b621c9
Rich2 <rr1@tutanota.com>
2020-07-05 17:48:29 +0100

    Minor.

--- src/Main.scala
+++ src/Main.scala
@@ -1,14 +1,12 @@
+package pMy
 import ostrat._, geom._
 
 object MyApp extends App
-{
-  def ds(d: Double): String = d.toLong match {
-    case l if d == l => l.toString
-    case _ => d.toString  
-  }
-
-  val v1 = 4 vv 5
-  println(v1.toString)
-  println(ds(v1.x))
-  println(ds(4.2))
+{ val v1 = 4 vv 5
+  val v2 = 4.2 vv 7
+  println(v1)
+  println(v2)
+  println(v1.x.str)
+  println(4.2.str)
+  //deb("ddf")
 } 
